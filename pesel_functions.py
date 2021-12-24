7c9717b40605bfc5811c63158a4913b0424e9c46
Kaluza05 <87182387+Kaluza05@users.noreply.github.com>
2021-12-24 13:46:09 +0100

    Add files via upload

---
+++ pesel_functions.py
@@ -0,0 +1,113 @@
+from random import randint
+import names
+import pandas as pd
+from pandas.core.frame import DataFrame
+def pesel():
+    year = randint(1900,2099)
+    month = randint(1,12)
+    day31_months = [1,3,5,7,8,10,12]
+    day30_months = [4,6,9,11]
+    if year>=2000:
+        month +=20
+        day31_months = [i+20 for i in day31_months]
+        day30_months = [i+20 for i in day30_months]
+    if month % 20  ==  2: 
+        if year % 4 == 0: # sprawdza lata przestepne
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    day_range = 29
+                else:
+                    day_range = 28
+            else:
+                day_range = 29
+        else:
+            day_range = 28
+        day = randint(1,day_range)
+    elif month in day31_months:
+        day = randint(1,31)
+    elif month in day30_months:
+        day = randint(1,30)
+    RR = str(year)[-2:] 
+    MM = str(month).zfill(2)
+    DD = str(day).zfill(2)
+    PPPP = ''.join([str(randint(0,9)) for _ in range(4)])
+    K = str(10 - int(str(sum([int(str(int(i)*wage)[-1]) for i,wage in zip(list(RR+MM+DD+PPPP),[1,3,7,9,1,3,7,9,1,3])]))[-1]))[-1]
+    return RR+MM+DD+PPPP+K
+
+def validate_pesel(pesel): #sprawdza czy liczba kontrolna się zgadza
+    if pesel[-1] == str(10 - int(str(sum([int(str(int(i)*wage)[-1]) for i,wage in zip(list(pesel[:-1]),[1,3,7,9,1,3,7,9,1,3])]))[-1]))[-1]:
+        return True
+    return False
+
+class NotFound(Exception): #wyjątek który ma być wywołany jeżeli wprowadzony pesel nie zostanie znaleziony
+    def __init__(self,spec,value, message = " not found on the list"):
+        self.spec = spec
+        self.value = value 
+        self.message = message
+        super().__init__(f'{self.spec}{self.message}: "{self.value}"')
+class WrongPesel(Exception): #wyjątek który ma byc wywołany jeżeli liczba kontrolna wprowadzonego peselu sie nie zgadza
+    def __init__(self, pesel,message = "Pesel that you provided doesn't exist or got repeated"):
+        self.pesel = pesel 
+        self.message = message
+        super().__init__(f'{self.message}: "{self.pesel}"')
+
+class Society: # tworzenie zbiorowiska obywateli 
+    def __init__(self,population):
+        self.population = population
+        self.citizens = { index : Citizen()  for index in range(1,population+1)} #słownik z peselem oraz 'adresem' klasy Citizen
+        self.society_dt = pd.DataFrame({
+         'indieces': self.citizens.keys(),
+         'name': [person.get_name() for person in self.citizens.values()],
+         'pesel': [person.get_pesel() for person in self.citizens.values()],
+         'gender' : [person.gender() for person in self.citizens.values()]})
+    def show_society(self): #wyświetla imię obywatela oraz jego pesel
+        return self.society_dt.to_string(index= False)
+    def society_dataframe(self):
+        return self.society_dt
+    def add_citizen(self): # dodaje nowego obywatela z losowo wygenerowanym peselem
+        self.new_citizen = Citizen()
+        self.new_dataframe = pd.DataFrame({ 
+         'indieces': self.population +1 ,
+         'name': [self.new_citizen.get_name()],
+         'pesel': [self.new_citizen.get_pesel()],
+         'gender' : [self.new_citizen.gender()] })
+        if self.new_dataframe['pesel'].to_string(index=False) not in self.society_dt['pesel'].tolist():
+            self.society_dt =  self.society_dt.append(self.new_dataframe,ignore_index=True)
+            self.population +=1
+
+    def ban_citizen(self,citizens_index): #usuwa obywatela ze społeczeństwa
+        if citizens_index in self.society_dt['indieces'].tolist():
+            self.population -= 1
+            self.society_dt = self.society_dt[self.society_dt['indieces'] != citizens_index].reset_index(drop=True)
+            self.society_dt['indieces'] = [i for i in range(1,self.count_population()+1)]
+        else: raise NotFound("Index",citizens_index)
+
+    def change_pesel(self,index,new_pesel): # podmienia stary pesel obywatela z nowo podanym
+        if len(new_pesel) <= 11 and validate_pesel(new_pesel) and new_pesel not in self.society_dt["pesel"].tolist():
+            if index in self.society_dt['indieces'].tolist():
+                self.society_dt.at[index-1,'pesel'] = new_pesel
+            else: raise NotFound("Index",index)
+        else: raise WrongPesel(new_pesel)
+    def get_info(self,index):
+        return self.society_dt.drop("indieces",axis=1).loc[index-1].to_string()
+    def get_genders(self): #zwraca słownik z przyporządkowanymi płciami do peselu
+        return self.society_dt[['pesel','gender']].to_string(index=False)
+    def count_woman(self): # liczy ile jest kobiet w społeczeństwie
+        return self.society_dt['gender'].tolist().count('female')
+    def count_man(self): # liczy ile jest mężczyzn w społeczeństwie
+        return self.society_dt['gender'].tolist().count('male')
+    def count_population(self): #populacja społeczeństwa
+        return self.population
+
+class Citizen: # tworzenie każdego obywatela, przypisanie pesela oraz imienia
+    def __init__(self):
+        self.pesel = pesel()
+        self.name = names.get_full_name(gender= self.gender())
+    def get_name(self):
+        return self.name
+    def get_pesel(self):
+        return self.pesel
+    def gender(self):
+        if int(self.pesel[-2]) % 2 == 0:
+            return 'female'
+        return 'male'
---
+++ pesel_main.py
@@ -0,0 +1,48 @@
+from pesel_functions import *
+
+def main():
+    try:
+        citizens = Society(int(input('Enter number of citizens: ')))
+    except:
+        citizens = Society(randint(5,20))
+        print(f"You input for number of citizens wasn't a number. We randomly generated size of society for you: {citizens.count_population()}")
+    while True:
+        action = input('''\nWhat do you want to do:
+        1 add_citizen  | 2 delete_citizen   | 3 change_pesel
+        4 show_society | 5 count_population | 6 count_man
+        7 count_woman  | 8 citizen_info     | 9 terminate_program
+        Command: ''')
+        print('\n')
+        if action == '1' or action == 'add_citizen':
+            citizens.add_citizen()
+            print(f'''citizen was added:\n{citizens.society_dataframe()[['name','pesel']].iloc[citizens.count_population()-1].to_string()} ''')
+        elif action == '2' or action == 'delete_citizen':
+            print(citizens.show_society())
+            to_delete = int(input('Which citizen do you want to delete from society?(Enter index) '))
+            print(f'\nCitizen has been deleated:\n{citizens.society_dataframe().drop("indieces",axis=1).loc[to_delete-1].to_string()}')
+            citizens.ban_citizen(to_delete)
+        elif action == '3' or action == 'change_pesel':
+            print(citizens.show_society())
+            index = int(input("Which person's pesel do you want to change?(Enter index) "))
+            old = citizens.society_dataframe().at[index-1,'pesel']
+            new = input('What is your desired new pesel? ')
+            citizens.change_pesel(index,new)
+            print(f"{citizens.society_dataframe().at[index-1,'name']}'s pesel changed.\n old: {old}\n new: {new}")
+        elif action == '4' or action == 'show_society':
+            print(citizens.show_society())
+        elif action == '5' or action == 'count_population':
+            print(f"Society's population right now is: {citizens.count_population()}")
+        elif action == '6' or action == 'count_man':
+            print(f"Number of males in society: {citizens.count_man()}.")
+        elif action == '7' or action == 'count_woman':
+            print(f"Number of females in society: {citizens.count_woman()}.")
+        elif action == '8' or action == 'citizen_info':
+            print(citizens.show_society())
+            to_show = int(input('Enter index of a citizen to show: '))
+            print('\n',citizens.get_info(to_show))
+        elif action == '9' or action == 'terminate_program':
+            print('Program terminated.')
+            break
+
+if __name__ == '__main__':
+    main()
